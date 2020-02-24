a5e15d0ceab8c53d2d5dd56c834816fba41880da
Ognjen Stoisavljevic <stoisavljevic.ognjen@gmail.com>
2020-02-24 20:16:10 +0100

    feat(menu): change menu JSON

--- common/scripts.js
+++ common/scripts.js
@@ -240,30 +240,30 @@ function isExists(elem) {
         return false;
 }
 
-$.ajax({
-        url: 'pizzahot.json',
-        dataType: 'json',
-        success: function (data) {
-                var items = [];
+// $.ajax({
+//         url: 'pizzahot.json',
+//         dataType: 'json',
+//         success: function (data) {
+//                 var items = [];
 
-                $.each(data, function (key, val) {
+//                 $.each(data, function (key, val) {
 
-                        items.push('<li id="' + key + '">' + val + '</li>');
+//                         items.push('<li id="' + key + '">' + val + '</li>');
 
-                });
+//                 });
 
-                $('<ul/>', {
-                        'class': 'interest-list',
-                        html: items.join('')
-                }).appendTo('body');
+//                 $('<ul/>', {
+//                         'class': 'interest-list',
+//                         html: items.join('')
+//                 }).appendTo('body');
 
-        },
-        statusCode: {
-                404: function () {
-                        alert('There was a problem with the server.  Try again soon!');
-                }
-        }
-});
+//         },
+//         statusCode: {
+//                 404: function () {
+//                         alert('There was a problem with the server.  Try again soon!');
+//                 }
+//         }
+// });
 
 
 
